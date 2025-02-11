502387f38800a5c3502550ddd4a1eea9e4ebd88e
Sinoo <khoasinno@gmail.com>
2025-02-11 23:38:10 +0700

    get schedules in profile doctor

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Specialty;
+use App\Repository\ScheduleWorkRepository;
 use App\Repository\SpecialtyRepository;
 use App\Repository\UserRepository;
+use App\Service\ScheduleService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,10 +17,13 @@ class HomeController extends AbstractController
 {
     private EntityManagerInterface $em;
     private SpecialtyRepository $specialtyRepository;
-    public function __construct(EntityManagerInterface $em, SpecialtyRepository $specialtyRepository)
+    private ScheduleService $scheduleService;
+
+    public function __construct(EntityManagerInterface $em, SpecialtyRepository $specialtyRepository, ScheduleService $scheduleService)
     {
         $this->em = $em;
         $this->specialtyRepository = $specialtyRepository;
+        $this->scheduleService = $scheduleService;
     }
 
 
@@ -37,11 +42,84 @@ class HomeController extends AbstractController
     #[Route('/search/doctor', name: 'app_search_doctor')]
     public function searchDoctor(UserRepository $userRepository, Request $request): Response
     {
-        $specialtyId = $request->query->get('specialty');
-        $doctors = $userRepository->findBySpecialty($specialtyId);
+        // Lấy các tham số tìm kiếm từ yêu cầu
+        $name = $request->query->get('fullname');
+        $address = $request->query->get('address');
+        $specialty = $request->query->get('specialty');
 
+        $criteria = [];
+        if ($name) {
+            $criteria['name'] = $name;
+        }
+        if ($address) {
+            $criteria['address'] = $address;
+        }
+        if ($specialty) {
+            $criteria['specialty'] = $specialty;
+        }
+
+        // Tìm bác sĩ theo các tiêu chí
+        $doctors = $userRepository->findDoctorsByCriteria($criteria);
+
+        // Render kết quả tìm kiếm
         return $this->render('home/search_doctor.html.twig', [
             'doctors' => $doctors,
+            'count' => count($doctors), // Đếm số lượng bác sĩ tìm được
+            'search_address' => $address, // Lưu địa chỉ tìm kiếm
+            'search_specialty' => $specialty, // Lưu chuyên khoa tìm kiếm
         ]);
     }
+
+    #[Route('/home/doctor_profile', name: 'app_doctor_profile')]
+    public function doctorProfile(
+        Request $request, 
+        UserRepository $userRepository, 
+        ScheduleWorkRepository $scheduleWorkRepository
+    ): Response {
+        $id = $request->query->get('id');
+        $doctor = $userRepository->find($id);
+    
+        if (!$doctor) {
+            throw $this->createNotFoundException('Bác sĩ không tồn tại');
+        }
+    
+        // Lấy tất cả các ngày có lịch khám của bác sĩ
+        $availableDates = $scheduleWorkRepository->getAvailableDatesByDoctor($doctor);
+    
+        // Kiểm tra xem có ngày nào được chọn không
+        $selectedDate = $request->query->get('date');
+        if ($selectedDate) {
+            $selectedDate = \DateTime::createFromFormat('Y-m-d', $selectedDate);
+        } else {
+            // Nếu không có ngày chọn, mặc định lấy ngày đầu tiên có trong lịch bác sĩ
+            $selectedDate = !empty($availableDates) ? new \DateTime($availableDates[0]) : null;
+        }
+    
+        // Lấy các khung giờ làm việc của bác sĩ
+        $timeSlots = $selectedDate ? $scheduleWorkRepository->getTimeSlotsByDoctorAndDate($doctor, $selectedDate) : [];
+    
+        return $this->render('home/doctor_profile.html.twig', [
+            'doctor' => $doctor,
+            'availableDates' => $availableDates,
+            'selectedDate' => $selectedDate,
+            'timeSlots' => $timeSlots,
+        ]);
+    }
+    
+
+
+
+
+
+
+
+    // public function searchDoctor(UserRepository $userRepository, Request $request): Response
+    // {
+    //     $specialtyId = $request->query->get('specialty');
+    //     $doctors = $userRepository->findBySpecialty($specialtyId);
+
+    //     return $this->render('home/search_doctor.html.twig', [
+    //         'doctors' => $doctors,
+    //     ]);
+    // }
 }
--- src/Repository/ScheduleWorkRepository.php
+++ src/Repository/ScheduleWorkRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\ScheduleWork;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -15,29 +16,70 @@ class ScheduleWorkRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ScheduleWork::class);
     }
+    public function getAvailableDatesByDoctor(User $doctor): array
+{
+    if (!$doctor || !$doctor->getId()) {
+        throw new \InvalidArgumentException('Invalid doctor or missing ID');
+    }
 
-//    /**
-//     * @return ScheduleWork[] Returns an array of ScheduleWork objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    $dates = $this->createQueryBuilder('s')
+        ->select('s.date')
+        ->where('s.doctor = :doctor')
+        ->setParameter('doctor', $doctor)
+        ->groupBy('s.date')
+        ->orderBy('s.date', 'ASC')
+        ->getQuery()
+        ->getResult();
 
-//    public function findOneBySomeField($value): ?ScheduleWork
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    return array_map(fn($d) => $d['date']->format('Y-m-d'), $dates);
+}
+
+
+
+public function getTimeSlotsByDoctorAndDate(User $doctor, \DateTime $date): array
+{
+    $result = $this->createQueryBuilder('s')
+        ->select('s.timeSlots')
+        ->where('s.doctor = :doctor')
+        ->andWhere('s.date = :date')
+        ->setParameter('doctor', $doctor)
+        ->setParameter('date', $date->format('Y-m-d'))
+        ->getQuery()
+        ->getOneOrNullResult();
+
+    return $result && isset($result['timeSlots']) ? $result['timeSlots'] : [];
+}
+
+
+
+
+
+
+
+
+
+    //    /**
+    //     * @return ScheduleWork[] Returns an array of ScheduleWork objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('s.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?ScheduleWork
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
